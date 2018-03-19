7eae3ea21cba4bc3e820950145f4ceb0235b6a7a
Martin Wirz <martin.w.wirz@gmail.com>
2018-03-19 20:00:43 +0100

    Initial

---
+++ js/script.js
@@ -0,0 +1,50 @@
+(function ($, window, document, undefined)
+{
+	'use strict';
+	
+	$(function ()
+	{
+	    $("#mobileMenu").hide();
+	    $(".toggleMobile").click(function()
+	    {
+	    	$(this).toggleClass("active");
+	        $("#mobileMenu").slideToggle(500);
+	    });
+	});
+	$(window).on("resize", function()
+	{
+		if($(this).width() > 500)
+		{
+			$("#mobileMenu").hide();
+			$(".toggleMobile").removeClass("active");
+		}
+	});
+})(jQuery, window, document);
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
